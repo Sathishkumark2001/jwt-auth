dbfde22294aa52e1ca2597ad2bf08a69b852cc00
Sathishkumark2001 <sk7488@srmist.edu.in>
2022-12-21 10:23:43 +0530

    initial commit

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: [true, 'Please enter an email'],
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter a password'],
+        minlength: [6, 'Minimum password length is 6 characters']
+    }
+});
+
+userSchema.post('save', function (doc, next) {
+    console.log('new user was created and saved', doc);
+    next();
+
+});
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+userSchema.statics.login = async function (email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if (auth) {
+            return user;
+        }
+        throw Error('incorrect password');
+
+    }
+    throw Error('incorrect email');
+
+}
+const User = mongoose.model('user', userSchema);
+module.exports = User;
